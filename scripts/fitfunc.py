360f7bc93d85c3afdbf7b5af3e968808f5c0a7a3
Tommaso Bendinelli <tbe@csem.ch>
2021-03-19 17:10:13 +0100

    changed method name

---
+++ scripts/fitfunc.py
@@ -0,0 +1,59 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+import time
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning import Trainer, seed_everything
+from dataclasses import dataclass
+from typing import Tuple
+from nesymres.architectures.model import Model
+from nesymres.architectures.data import DataModule
+from nesymres.dclasses import Params, DataModuleParams
+seed_everything(9)
+from nesymres.utils import load_data
+
+
+def main():
+    model_path = "Exp_weights/data/datasets/100K"
+    test_data = load_data("data/datasets/100K/100K_val_subset")
+    params = Params(datamodule_params_train=DataModuleParams(
+                                total_variables=list(train_data.total_variables), 
+                                total_coefficients=list(train_data.total_coefficients)),
+                    datamodule_params_val=DataModuleParams(
+                        total_variables=list(val_data.total_variables), 
+                        total_coefficients=list(train_data.total_coefficients)))
+    data = DataModule(
+        train_data,
+        val_data,
+        None,
+        cfg=params
+    )
+    model = Model.load_from_checkpoint()
+    model = Model(model_path, cfg=params.architecture)
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss", #/dataloader_idx_0",
+        dirpath="Exp_weights/",                 
+        filename=str(train_path)+"_log_"+"-{epoch:02d}-{val_loss:.2f}",
+        mode="min",
+    )
+
+    trainer = pl.Trainer(
+        distributed_backend="ddp",
+        gpus=-1,
+        max_epochs=params.max_epochs,
+        val_check_interval=params.val_check_interval,
+        callbacks=[checkpoint_callback],
+        #precision=params.precision,
+    )
+    trainer.fit(model, data)
+
+
+if __name__ == "__main__":
+    #os.environ["CUDA_VISIBLE_DEVICES"] = "0,2"  # ,1,2,4,5,6,7" Change Me
+    #print(f"Starting a run with {config}")
+    main()
